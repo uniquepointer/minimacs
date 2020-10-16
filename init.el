07511c118dccfcab57a44ffc9ffa0af7b07e2dc3
hermesboo <71751817+hermesboo@users.noreply.github.com>
2020-10-16 13:43:09 -0500

    removing packages

--- init.el
+++ init.el
@@ -93,9 +93,6 @@
         emojify
         emojify-logos
         elpy
-        go-mode
-        go-playground
-        haskell-mode
         helpful
         highlight-indent-guides
         magit
@@ -114,24 +111,12 @@
 
 ;;Porgramming modes
 ; ---------------------------------------------------------------------------------
-;;haskell
-(eval-after-load "haskell-mode"
-    '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
-
-(require 'haskell-interactive-mode)
-(require 'haskell-process)
-(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-(add-hook 'prog-mode-hook 'paredit-everywhere-mode)
-
+;;Python
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
   
-(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
-(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
-(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-;;(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
-
 ;;ORG MODE
 ;; ---------------------------------------------------------------------------------
 (require 'org-superstar)
