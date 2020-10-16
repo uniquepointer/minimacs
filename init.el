67174208143b1f2772dd8f3f7ccb018b4033453c
hermesboo <71751817+hermesboo@users.noreply.github.com>
2020-10-16 08:18:26 -0500

    World first commit

---
+++ init.el
@@ -0,0 +1,209 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes '(doom-Iosvkem))
+ '(custom-safe-themes
+   '("990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "a3b6a3708c6692674196266aad1cb19188a6da7b4f961e1369a68f06577afa16" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "5b809c3eae60da2af8a8cfba4e9e04b4d608cb49584cb5998f6e4a1c87c057c4" "71e5acf6053215f553036482f3340a5445aee364fb2e292c70d9175fb0cc8af7" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "dde8c620311ea241c0b490af8e6f570fdd3b941d7bc209e55cd87884eb733b0e" "5036346b7b232c57f76e8fb72a9c0558174f87760113546d3a9838130f1cdb74" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "acb636fb88d15c6dd4432e7f197600a67a48fd35b54e82ea435d7cd52620c96d" "30b14930bec4ada72f48417158155bc38dd35451e0f75b900febd355cda75c3e" "3a78cae35163bb71df460ebcfdebf811fd7bc74eaa15428c7e0bccfd4f858d30" default))
+ '(elcord-mode t nil (elcord))
+ '(fci-rule-color "#767676")
+ '(fringe-mode 6 nil (fringe))
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(jdee-db-active-breakpoint-face-colors (cons "#0F1019" "#D85F00"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0F1019" "#79D836"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0F1019" "#767676"))
+ '(linum-format 'dynamic)
+ '(main-line-color1 "#222232")
+ '(main-line-color2 "#333343")
+ '(objed-cursor-color "#D83441")
+ '(package-selected-packages
+   '(treemacs-magit beacon go-playground go-mode magit treemacs-all-the-icons highlight-indent-guides emojify-logos emojify doom-modeline paredit-everywhere paredit projectile all-the-icons dashboard doom-themes treemacs elcord minibuffer-complete-cycle cherry-blossom-theme clues-theme helpful company-quickhelp company haskell-mode))
+ '(pdf-view-midnight-colors (cons "#CEDBE5" "#0D0E16"))
+ '(powerline-color1 "#222232")
+ '(powerline-color2 "#333343")
+ '(rustic-ansi-faces
+   ["#0D0E16" "#D83441" "#79D836" "#D8B941" "#3679D8" "#8041D8" "#36D8BD" "#CEDBE5"])
+ '(show-paren-mode t)
+ '(vc-annotate-background "#0D0E16")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#79D836")
+    (cons 40 "#98cd39")
+    (cons 60 "#b8c33d")
+    (cons 80 "#D8B941")
+    (cons 100 "#d89b2b")
+    (cons 120 "#d87d15")
+    (cons 140 "#D85F00")
+    (cons 160 "#ba5548")
+    (cons 180 "#9d4b90")
+    (cons 200 "#8041D8")
+    (cons 220 "#9d3ca5")
+    (cons 240 "#ba3873")
+    (cons 260 "#D83441")
+    (cons 280 "#bf444e")
+    (cons 300 "#a7555b")
+    (cons 320 "#8e6568")
+    (cons 340 "#767676")
+    (cons 360 "#767676")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif" :height 1.5 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif")))))
+
+;;Package shit ----------------------
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;;dependencies
+;--------------------------------------------------------
+;; Declare packages
+(setq my-packages
+      '(all-the-icons
+        beacon
+        cherry-blossom-theme
+        clues-theme
+        company
+        company-quickhelp
+        dashboard
+        doom-modeline
+        doom-themes
+        elcord
+        emojify
+        emojify-logos
+        go-mode
+        go-playground
+        haskell-mode
+        helpful
+        highlight-indent-guides
+        magit
+        minibuffer-complete-cycle
+        paredit
+        paredit-everywhere
+        projectile
+        treemacs
+        treemacs-all-the-icons
+        treemacs-magit))
+
+;; Iterate on packages and install missing ones
+(dolist (pkg my-packages)
+  (unless (package-installed-p pkg)
+    (package-install pkg)))
+
+;; (desktop-save-mode 1)
+
+;; Visual stuff
+;; --------------
+(require 'dashboard)
+(dashboard-setup-startup-hook)
+(setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
+;; Set the title
+(setq dashboard-banner-logo-title "Welcome to Emacs Dashboard")
+;; Set the banner
+(setq dashboard-startup-banner "~/.emacs.d/epics/vodka.png")
+;; Value can be
+;; 'official which displays the official emacs logo
+;; 'logo which displays an alternative emacs logo
+;; 1, 2 or 3 which displays one of the text banners
+;; "path/to/your/image.png" which displays whatever image you would prefer
+
+;; Content is not centered by default. To center, set
+(setq dashboard-center-content t)
+(beacon-mode 1)
+
+;; Key stuff
+;; --------------
+;; To disable shortcut "jump" indicators for each section, set
+(setq dashboard-show-shortcuts nil)
+(global-display-line-numbers-mode)
+(electric-pair-mode)
+(autoload 'enable-paredit-mode "paredit"
+  "Turn on pseudo-structural editing of Lisp code."
+  t)
+(add-hook 'emacs-lisp-mode-hook       'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'haskell-mode-hook           'enable-paredit-mode)
+
+;;haskell shit 
+;;--------------
+(eval-after-load "haskell-mode"
+    '(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
+
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+(add-hook 'prog-mode-hook 'paredit-everywhere-mode)
+
+  
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+;;(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+
+
+;;doom modeline 
+;;---------------
+(require 'doom-modeline)
+(doom-modeline-mode 1)
+
+;;emojify
+;; ---------
+(add-hook 'after-init-hook #'global-emojify-mode)
+
+;;indentantion
+;; ---------
+(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+(setq highlight-indent-guides-method 'column)
+
+;;highlight
+;;------
+
+;;ORG MODE
+;; -------------------------
+(require 'org-superstar)
+(add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+
+(let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+                             ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+                             ((x-list-fonts "Verdana")         '(:font "Verdana"))
+                             ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+                             (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+       (base-font-color     (face-foreground 'default nil 'default))
+       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+
+  (custom-theme-set-faces 'user
+                          `(org-level-8 ((t (,@headline ,@variable-tuple))))
+                          `(org-level-7 ((t (,@headline ,@variable-tuple))))
+                          `(org-level-6 ((t (,@headline ,@variable-tuple))))
+                          `(org-level-5 ((t (,@headline ,@variable-tuple))))
+                          `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+                          `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+                          `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+                          `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+                          `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
+
+(setq org-hide-emphasis-markers t)                          
