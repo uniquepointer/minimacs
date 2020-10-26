366265c2f92d27666b76484092d4664c5c0d88cf
hermesboo <71751817+hermesboo@users.noreply.github.com>
2020-10-26 13:24:45 -0500

    patchesss

--- init.el
+++ init.el
@@ -1,80 +1,18 @@
-;;Shit I did not set
-;; 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes '(doom-horizon))
- '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "c4bdbbd52c8e07112d1bfd00fee22bf0f25e727e95623ecb20c4fa098b74c1bd" "74ba9ed7161a26bfe04580279b8cad163c00b802f54c574bfa5d924b99daa4b9" "8d7684de9abb5a770fbfd72a14506d6b4add9a7d30942c6285f020d41d76e0fa" "f7216d3573e1bd2a2b47a2331f368b45e7b5182ddbe396d02b964b1ea5c5dc27" "3df5335c36b40e417fec0392532c1b82b79114a05d5ade62cfe3de63a59bc5c6" "711efe8b1233f2cf52f338fd7f15ce11c836d0b6240a18fffffc2cbd5bfe61b0" "7b3d184d2955990e4df1162aeff6bfb4e1c3e822368f0359e15e2974235d9fa8" "5d09b4ad5649fea40249dd937eaaa8f8a229db1cec9a1a0ef0de3ccf63523014" "7d708f0168f54b90fc91692811263c995bebb9f68b8b7525d0e2200da9bc903c" "c83c095dd01cde64b631fb0fe5980587deec3834dc55144a6e78ff91ebc80b19" "730a87ed3dc2bf318f3ea3626ce21fb054cd3a1471dcd59c81a4071df02cb601" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "a3b6a3708c6692674196266aad1cb19188a6da7b4f961e1369a68f06577afa16" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "e074be1c799b509f52870ee596a5977b519f6d269455b84ed998666cf6fc802a" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "5b809c3eae60da2af8a8cfba4e9e04b4d608cb49584cb5998f6e4a1c87c057c4" "71e5acf6053215f553036482f3340a5445aee364fb2e292c70d9175fb0cc8af7" "1623aa627fecd5877246f48199b8e2856647c99c6acdab506173f9bb8b0a41ac" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "dde8c620311ea241c0b490af8e6f570fdd3b941d7bc209e55cd87884eb733b0e" "5036346b7b232c57f76e8fb72a9c0558174f87760113546d3a9838130f1cdb74" "188fed85e53a774ae62e09ec95d58bb8f54932b3fd77223101d036e3564f9206" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "acb636fb88d15c6dd4432e7f197600a67a48fd35b54e82ea435d7cd52620c96d" "30b14930bec4ada72f48417158155bc38dd35451e0f75b900febd355cda75c3e" "3a78cae35163bb71df460ebcfdebf811fd7bc74eaa15428c7e0bccfd4f858d30" default))
- '(fci-rule-color "#767676")
- '(fringe-mode 6 nil (fringe))
- '(haskell-process-auto-import-loaded-modules t)
- '(haskell-process-log t)
- '(haskell-process-suggest-remove-import-lines t)
- '(jdee-db-active-breakpoint-face-colors (cons "#0F1019" "#D85F00"))
- '(jdee-db-requested-breakpoint-face-colors (cons "#0F1019" "#79D836"))
- '(jdee-db-spec-breakpoint-face-colors (cons "#0F1019" "#767676"))
- '(linum-format 'dynamic)
- '(main-line-color1 "#222232")
- '(main-line-color2 "#333343")
- '(objed-cursor-color "#D83441")
- '(package-selected-packages
-   '(treemacs-projectile neotree spacemacs-theme irony lsp-mode nim-mode treemacs-magit beacon go-playground go-mode magit treemacs-all-the-icons highlight-indent-guides emojify-logos emojify doom-modeline paredit-everywhere paredit projectile all-the-icons dashboard doom-themes treemacs minibuffer-complete-cycle cherry-blossom-theme clues-theme helpful company-quickhelp company haskell-mode))
- '(pdf-view-midnight-colors (cons "#CEDBE5" "#0D0E16"))
- '(powerline-color1 "#222232")
- '(powerline-color2 "#333343")
- '(rustic-ansi-faces
-   ["#0D0E16" "#D83441" "#79D836" "#D8B941" "#3679D8" "#8041D8" "#36D8BD" "#CEDBE5"])
- '(show-paren-mode t)
- '(vc-annotate-background "#0D0E16")
- '(vc-annotate-color-map
-   (list
-    (cons 20 "#79D836")
-    (cons 40 "#98cd39")
-    (cons 60 "#b8c33d")
-    (cons 80 "#D8B941")
-    (cons 100 "#d89b2b")
-    (cons 120 "#d87d15")
-    (cons 140 "#D85F00")
-    (cons 160 "#ba5548")
-    (cons 180 "#9d4b90")
-    (cons 200 "#8041D8")
-    (cons 220 "#9d3ca5")
-    (cons 240 "#ba3873")
-    (cons 260 "#D83441")
-    (cons 280 "#bf444e")
-    (cons 300 "#a7555b")
-    (cons 320 "#8e6568")
-    (cons 340 "#767676")
-    (cons 360 "#767676")))
- '(vc-annotate-very-old-color nil))
- 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif" :height 1.5 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif" :height 1.75))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif" :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif" :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#dddddd" :family "Sans Serif")))))
- ;; 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 
 ;;Packages
 ;; 
 (require 'package)
 
 ; list the packages you want
-(setq package-list '(org-superstar all-the-icons use-package elpy beacon cherry-blossom-theme clues-theme company company-quickhelp dashboard doom-modeline doom-themes emojify emojify-logos helpful highlight-indent-guides magit minibuffer-complete-cycle paredit paredit-everywhere projectile treemacs treemacs-all-the-icons treemacs-magit))
+(setq package-list '(yafolding org-superstar all-the-icons use-package irony nim-mode 
+beacon cherry-blossom-theme vterm clues-theme company company-quickhelp dashboard 
+doom-modeline doom-themes emojify emojify-logos go-mode go-playground 
+haskell-mode helpful highlight-indent-guides magit minibuffer-complete-cycle 
+paredit paredit-everywhere projectile treemacs treemacs-all-the-icons 
+treemacs-magit rainbow-delimiters toc-org company-irony flycheck-irony company-irony-c-headers))
 
 ; list the repositories containing them
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -82,6 +20,7 @@
 
 ; activate all the packages (in particular autoloads)
 (package-initialize)
+
 
 ; fetch the list of packages available 
 (unless package-archive-contents
@@ -113,7 +52,7 @@
                              ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
                              (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
        (base-font-color     (face-foreground 'default nil 'default))
-       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+       (headline           `(:inherit default :weight bold :foreground "white")))
 
   (custom-theme-set-faces 'user
                           `(org-level-8 ((t (,@headline ,@variable-tuple))))
@@ -127,6 +66,15 @@
                           `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
 
 (setq org-hide-emphasis-markers t)
+
+;TOC 
+(if (require 'toc-org nil t)
+    (add-hook 'org-mode-hook 'toc-org-mode)
+
+    ;; enable in markdown, too
+    (add-hook 'markdown-mode-hook 'toc-org-mode)
+    (define-key markdown-mode-map (kbd "\C-c\C-o") 'toc-org-markdown-follow-thing-at-point)
+  (warn "toc-org not found"))
 
 ;;Random editing stuff
 ; 
@@ -184,3 +132,42 @@
 (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
 (setq highlight-indent-guides-method 'column)
 
+;;company mode
+;
+(add-hook 'after-init-hook 'global-company-mode)
+
+;;yafolding
+
+;;yafolding keybinds
+(defvar yafolding-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "<C-S-return>") #'yafolding-hide-parent-element)
+    (define-key map (kbd "<C-M-return>") #'yafolding-toggle-all)
+    (define-key map (kbd "<C-return>") #'yafolding-toggle-element)
+    map))
+;;end of yafolding keybinds
+
+
+;custom yafolding keybinds
+ 
+;(require 'yafolding)
+;(define-key yafolding-mode-map (kbd "<C-S-return>") nil)
+;(define-key yafolding-mode-map (kbd "<C-M-return>") nil)
+;(define-key yafolding-mode-map (kbd "<C-return>") nil)
+;(define-key yafolding-mode-map (kbd "C-c <C-M-return>") 'yafolding-toggle-all)
+;(define-key yafolding-mode-map (kbd "C-c <C-S-return>") 'yafolding-hide-parent-element)
+;(define-key yafolding-mode-map (kbd "C-c <C-return>") 'yafolding-toggle-element)
+
+;end of custom yafolding keybinds
+
+(add-hook 'prog-mode-hook
+          (lambda () (yafolding-mode)))
+
+;;rainbow delimiters
+;
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(show-paren-mode 1)
+
+;;Global custom keybinds
+
+(global-set-key (kbd "C-x p") 'move-to-window-line-top-bottom)
